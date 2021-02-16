33f1b9fd20325535f3ebc97efbd29c73c8664d98
sugarnyang <72382334+sugarnyang@users.noreply.github.com>
2021-02-16 23:40:01 +0900

    #21 데이터 Flow & Axios
    loginpage.js 에 require('express') 줄 지워보고 실행해보세요.
    
    저도 그거로 어제 1시간 날렸네요. 브라우저에서는 node.js기반의 익스프레스 모듈을 인식하지 못한답니다.
    
    https://www.inflearn.com/questions/30903

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios'
 
 function LandingPage() {
+    //랜딩페이지에 들어오자마자 실행한다.
+    useEffect(() => {
+        axios.get('http://localhost:5000/api/hello')
+        .then(response => console.log(response.data))
+    }, [])
+
     return (
         <div>
             LandingPage 랜딩페이지
--- server/index.js
+++ server/index.js
@@ -24,6 +24,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!~~ nodemon을 적용했습니다.')
 })
 
+app.get('/api/hello', (req, res) => {
+  res.send('/api/hello 입니다.')
+})
+
 app.post('/api/users/register', (req, res) => {
 
   // 회원 가입 할 때 필요한 정보들을 client에서 가져오면
