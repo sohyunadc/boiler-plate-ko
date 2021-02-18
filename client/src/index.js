09ee28ca25a9b60c0241944c1ca38e116e7c43e2
sugarnyang <72382334+sugarnyang@users.noreply.github.com>
2021-02-18 23:20:17 +0900

    #27 Redux UP !!!!!
    따라하긴 했는데 솔직히 아직은 잘 모르겠다

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { Provider } from 'react-redux'; // 여기가 앱이 시작되는 곳이고 여기에 리덕스를 연결해주어야 한다.
 import 'antd/dist/antd.css';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers';
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={createStoreWithMiddleware(Reducer, 
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()
+  )}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
