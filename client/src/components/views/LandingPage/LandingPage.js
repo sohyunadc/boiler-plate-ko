a7df1d251127dc047a422303c7ee3059b84bb2a0
sugarnyang <72382334+sugarnyang@users.noreply.github.com>
2021-02-21 01:38:23 +0900

    #32 로그아웃
    onClickHandler 에서 로그아웃을 했을때 /login으로 요청하는 부분에서 Cannot read property 'push' of undefined 가 자꾸 떴는데 props의 위치가 틀렸다. function LandingPage(props) 여기 써줬어야 했다. 나는 const onClickHandler = (props) 여기에 넣었었다.

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,12 +1,27 @@
 import React, { useEffect } from 'react'
 import axios from 'axios'
 
-function LandingPage() {
+function LandingPage(props) {
     //랜딩페이지에 들어오자마자 실행한다.
     useEffect(() => {
         axios.get('/api/hello')
-            .then(response => console.log(response.data))
+            .then(response => { 
+                console.log(response.data) 
+            })
     }, [])
+
+    const onClickHandler = () => {
+        axios.get('/api/users/logout')
+            .then(response => {
+                console.log(response.data)
+
+                if (response.data.success) {
+                    props.history.push('/login')
+                } else {
+                    alert('로그아웃 실패!')
+                }
+            })
+    }
 
     return (
         <div style={{
@@ -14,6 +29,10 @@ function LandingPage() {
             , width: '100%', height: '100vh'
         }}>
             <h2>시작 페이지</h2>
+
+            <button onClick={onClickHandler}>
+                로그아웃
+            </button>
         </div>
     )
 }
